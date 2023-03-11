d1e6c6880346782e5945c162ce02e52199d54c5e
twk <1024565378@qq.com>
2023-03-12 02:01:49 +0800

    !

--- block/data_get.py
+++ block/data_get.py
@@ -9,34 +9,34 @@ def data_get(args):
 
 class data_prepare(object):
     def __init__(self, args):
-        self.args = args
+        self.input_column = args.input_column
+        self.output_column = args.output_column
+        self.divide = args.divide
+        self.data_path = args.data_path
 
     def _load(self):
-        input_column = self.args.input_column
-        output_column = self.args.output_column
-        divide = self.args.divide
         # 读取数据
-        df = pd.read_csv(self.args.data_path)
-        input_data = np.array(df[input_column].astype(np.float32))
-        output_data = np.array(df[output_column].astype(np.float32))
+        df = pd.read_csv(self.data_path)
+        input_data = np.array(df[self.input_column].astype(np.float32))
+        output_data = np.array(df[self.output_column].astype(np.float32))
         # 划分数据集
-        boundary = int(len(df) * divide[0] / (divide[0] + divide[1]))
+        boundary = int(len(df) * self.divide[0] / (self.divide[0] + self.divide[1]))
         train_input = input_data[0:boundary]  # 训练数据
         train_output = output_data[0:boundary]  # 训练标签
         val_input = input_data[boundary:len(df)]  # 验证数据
         val_output = output_data[boundary:len(df)]  # 验证标签
         # 记录输入数据的处理方式(数据处理在模型中完成)
-        input_mean = np.zeros(len(input_column))
-        input_std = np.zeros(len(input_column))
-        for i in range(len(input_column)):
+        input_mean = np.zeros(len(self.input_column))
+        input_std = np.zeros(len(self.input_column))
+        for i in range(len(self.input_column)):
             mean = np.mean(train_input[:, i])
             std = np.std(train_input[:, i])
             input_mean[i] = mean
             input_std[i] = std
         # 记录输出数据的处理方式(数据处理在模型中完成)
-        output_mean = np.zeros(len(output_column))
-        output_std = np.zeros(len(output_column))
-        for i in range(len(output_column)):
+        output_mean = np.zeros(len(self.output_column))
+        output_std = np.zeros(len(self.output_column))
+        for i in range(len(self.output_column)):
             mean = np.mean(train_output[:, i])
             std = np.std(train_output[:, i])
             output_mean[i] = mean
@@ -58,10 +58,12 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--data_path', default='../dataset/demo.csv', type=str)
-    parser.add_argument('--input_column', default='1,3', type=str)
-    parser.add_argument('--output_column', default='1,3', type=str)
-    parser.add_argument('--device', default='9,1', type=str)
+    parser.add_argument('--data_path', default='../dataset/ETTh.csv', type=str)
+    parser.add_argument('--input_column', default='1,2,3', type=str)
+    parser.add_argument('--output_column', default='1,2', type=str)
+    parser.add_argument('--divide', default='9,1', type=str)
     args = parser.parse_args()
+    args.input_column = args.input_column.split(',')
+    args.output_column = args.output_column.split(',')
     args.divide = list(map(int, args.divide.split(',')))
     data_dict = data_get(args)
---
+++ test_pt.py
@@ -0,0 +1,104 @@
+import os
+import time
+import torch
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# 设置
+parser = argparse.ArgumentParser(description='pt模型推理')
+parser.add_argument('--model_path', default='best.pt', type=str, help='|pt模型位置|')
+parser.add_argument('--data_path', default=r'./dataset/ETTh.csv', type=str, help='|数据路径|')
+parser.add_argument('--divide', default='9,1', type=str, help='|训练集和验证集划分比例|')
+parser.add_argument('--input_column', default='1,2,3', type=str, help='|选择输入的变量|')
+parser.add_argument('--output_column', default='1,2', type=str, help='|选择预测的变量|')
+parser.add_argument('--input_size', default=128, type=int, help='|输入的长度|')
+parser.add_argument('--output_size', default=32, type=int, help='|输出的长度|')
+parser.add_argument('--batch', default=64, type=int, help='|训练批量大小|')
+parser.add_argument('--device', default='cuda', type=str, help='|用CPU/GPU推理|')
+parser.add_argument('--num_worker', default=0, type=int, help='|CPU在处理数据时使用的进程数，0表示只有一个主进程，一般为0、2、4、8|')
+args = parser.parse_args()
+args.model_path = args.model_path.split('.')[0] + '.pt'
+args.input_column = args.input_column.split(',')
+args.output_column = args.output_column.split(',')
+args.divide = list(map(int, args.divide.split(',')))
+# -------------------------------------------------------------------------------------------------------------------- #
+# 初步检查
+assert os.path.exists(args.model_path), f'没有找到模型{args.model_path}'
+assert os.path.exists(args.data_path), f'没有找到预测数据{args.data_path}'
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# 程序
+def draw(pred, true, number):  # pred为模型输出，true为真实数据，pred和true长度不相等
+    # 画图
+    x = np.arange(len(true) + number)
+    y_pred = np.zeros(len(true) + number)
+    n = len(x) // 300
+    n += 1 if len(x) % 300 else 0
+    for i in range(len(args.output_column)):
+        y_pred[args.input_size + number - 1:] = pred[:, i]
+        y_true = true[:, i]
+        for j in range(n):
+            plt.title(args.output_column[i] + f':{300 * j}-{300 * (j + 1)}(number:{number}).jpg')
+            plt.plot(y_pred[300 * j:300 * (j + 1)], color='cyan')
+            plt.plot(y_true[300 * j:300 * (j + 1)], color='green')
+            plt.savefig(args.output_column[i] + f'_{300 * j}-{300 * (j + 1)}(number_{number}).jpg')
+            plt.show()
+
+
+def test_pt():
+    # 加载模型
+    model_dict = torch.load(args.model_path, map_location='cpu')
+    model = model_dict['model']
+    model.float().eval().to(args.device)
+    print('| 模型加载成功:{} |'.format(args.model_path))
+    # 加载数据
+    df = pd.read_csv(args.data_path)
+    input_data = np.array(df[args.input_column].astype(np.float32))[-600 + args.output_size:]  # 限定长度方便画图
+    output_data = np.array(df[args.output_column].astype(np.float32))[-600 + args.output_size:]  # 限定长度方便画图
+    # 推理
+    middle = args.output_size // 2
+    last = args.output_size
+    start_time = time.time()
+    with torch.no_grad():
+        dataloader = torch.utils.data.DataLoader(torch_dataset(args, input_data), batch_size=args.batch,
+                                                 shuffle=False, drop_last=False, pin_memory=False,
+                                                 num_workers=args.num_worker)
+        result_middle = []
+        result_last = []
+        for item, batch in enumerate(dataloader):
+            batch = batch.to(args.device)
+            pred_batch = model(batch).detach().cpu().numpy()
+            result_middle.append(pred_batch[:, :, middle - 1])
+            result_last.append(pred_batch[:, :, last - 1])
+        result_middle = np.concatenate(result_middle, axis=0)
+        result_last = np.concatenate(result_last, axis=0)
+    end_time = time.time()
+    print('| 数据:{} 批量:{} 平均耗时:{:.4f} |'
+          .format(args.data_path, args.batch, (end_time - start_time) / len(result_middle)))
+    # 画图
+    draw(result_middle, output_data, middle)
+    draw(result_last, output_data, last)
+
+
+class torch_dataset(torch.utils.data.Dataset):
+    def __init__(self, args, input_data):
+        self.input_size = args.input_size
+        self.output_size = args.output_size
+        self.input_data = input_data
+
+    def __len__(self):
+        return len(self.input_data) - self.input_size + 1
+
+    def __getitem__(self, index):
+        boundary = index + self.input_size
+        series = self.input_data[index:boundary]  # 输入数据
+        series = torch.tensor(series, dtype=torch.float32).permute(1, 0)  # 转换为tensor
+        return series
+
+
+if __name__ == '__main__':
+    test_pt()
