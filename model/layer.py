195298aabf93ad9a9718c11f4665adc736c238d4
twk <1024565378@qq.com>
2023-03-29 14:54:04 +0800

    !

--- model/layer.py
+++ model/layer.py
@@ -28,7 +28,8 @@ class series_decode(torch.nn.Module):
 class cbs(torch.nn.Module):
     def __init__(self, in_, out_, kernel_size, stride):
         super().__init__()
-        self.conv = torch.nn.Conv1d(in_, out_, kernel_size=kernel_size, stride=stride, padding=(kernel_size - 1) // 2)
+        self.conv = torch.nn.Conv1d(in_, out_, kernel_size=kernel_size, stride=stride, padding=(kernel_size - 1) // 2,
+                                    bias=False)
         self.bn = torch.nn.BatchNorm1d(out_)
         self.silu = torch.nn.SiLU()
 
