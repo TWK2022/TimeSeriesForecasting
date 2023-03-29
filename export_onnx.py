5c29a6738943b1d0664e444e4729bbe0d6d664a7
twk <1024565378@qq.com>
2023-03-29 22:41:50 +0800

    !

---
+++ export_onnx.py
@@ -0,0 +1,51 @@
+import os
+import torch
+import argparse
+from model.layer import deploy
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# 设置
+parser = argparse.ArgumentParser(description='将pt模型字典中的模型转为onnx，同时导出类别信息')
+parser.add_argument('--weight', default='best.pt', type=str, help='|模型位置|')
+parser.add_argument('--input_size', default=128, type=int, help='|输入的长度|')
+parser.add_argument('--input_column_num', default=3, type=int, help='|输入的变量数量|')
+parser.add_argument('--batch', default=0, type=int, help='|输入图片批量，0为动态|')
+parser.add_argument('--sim', default=True, type=bool, help='|使用onnxsim压缩简化模型|')
+parser.add_argument('--device', default='cuda', type=str, help='|在哪个设备上加载模型|')
+parser.add_argument('--float16', default=True, type=bool, help='|转换的onnx模型数据类型，需要GPU，False时为float32|')
+args = parser.parse_args()
+args.weight = args.weight.split('.')[0] + '.pt'
+args.save_name = args.weight.split('.')[0] + '.onnx'
+# -------------------------------------------------------------------------------------------------------------------- #
+# 初步检查
+assert os.path.exists(args.weight), f'没有找到模型{args.weight}'
+if args.float16:
+    assert torch.cuda.is_available(), 'cuda不可用，因此无法使用float16'
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# 程序
+def export_onnx():
+    model_dict = torch.load(args.weight, map_location='cpu')
+    model = model_dict['model']
+    model = deploy(model, model_dict['input_mean'], model_dict['input_std'], model_dict['output_mean'],
+                   model_dict['output_std'])
+    model.eval().half().to(args.device) if args.float16 else model.eval().float().to(args.device)
+    input_shape = torch.rand(1, args.input_column_num, args.input_size,
+                             dtype=torch.float16 if args.float16 else torch.float32).to(args.device)
+    torch.onnx.export(model, input_shape, args.save_name,
+                      opset_version=12, input_names=['input'], output_names=['output'],
+                      dynamic_axes={'input': {args.batch: 'batch_size'}, 'output': {args.batch: 'batch_size'}})
+    print(f'| 转为onnx模型成功:{args.save_name} |')
+    if args.sim:
+        import onnx
+        import onnxsim
+
+        model_onnx = onnx.load(args.save_name)
+        model_simplify, check = onnxsim.simplify(model_onnx)
+        onnx.save(model_simplify, args.save_name)
+        print(f'| 使用onnxsim简化模型成功:{args.save_name} |')
+
+
+if __name__ == '__main__':
+    export_onnx()
--- test_onnx.py
+++ test_onnx.py
@@ -1,24 +1,23 @@
 import os
 import time
-import torch
 import argparse
+import onnxruntime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from model.layer import deploy
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # 设置
-parser = argparse.ArgumentParser(description='pt模型推理')
-parser.add_argument('--model_path', default='best.pt', type=str, help='|pt模型位置|')
+parser = argparse.ArgumentParser(description='onnx推理')
+parser.add_argument('--model_path', default='best.onnx', type=str, help='|onnx模型位置|')
 parser.add_argument('--data_path', default=r'./dataset/ETTh.csv', type=str, help='|数据路径|')
 parser.add_argument('--input_column', default='1,2,3', type=str, help='|选择输入的变量|')
 parser.add_argument('--output_column', default='1,2,3', type=str, help='|选择预测的变量|')
 parser.add_argument('--input_size', default=128, type=int, help='|输入的长度|')
 parser.add_argument('--output_size', default=32, type=int, help='|输出的长度|')
-parser.add_argument('--batch', default=64, type=int, help='|训练批量大小|')
+parser.add_argument('--batch', default=2, type=int, help='|输入图片批量，要与导出的模型对应|')
 parser.add_argument('--device', default='cuda', type=str, help='|用CPU/GPU推理|')
-parser.add_argument('--num_worker', default=0, type=int, help='|CPU在处理数据时使用的进程数，0表示只有一个主进程，一般为0、2、4、8|')
+parser.add_argument('--float16', default=True, type=bool, help='|推理数据类型，要与导出的模型对应，False时为float32|')
 args = parser.parse_args()
 args.input_column = args.input_column.split(',')
 args.output_column = args.output_column.split(',')
@@ -51,59 +50,57 @@ def draw(pred, true, number):  # pred为模型输出，true为真实数据，pre
             plt.close()
 
 
-def test_pt():
+def test_onnx():
     # 加载模型
-    model_dict = torch.load(args.model_path, map_location='cpu')
-    model = model_dict['model']
-    model = deploy(model, model_dict['input_mean'], model_dict['input_std'], model_dict['output_mean'],
-                   model_dict['output_std'])
-    model.float().eval().to(args.device)
-    print('| 模型加载成功:{} |'.format(args.model_path))
+    provider = 'CUDAExecutionProvider' if args.device.lower() in ['gpu', 'cuda'] else 'CPUExecutionProvider'
+    session = onnxruntime.InferenceSession(args.model_path, providers=[provider])
+    input_name = session.get_inputs()[0].name
+    output_name = session.get_outputs()[0].name
+    print(f'| 模型加载成功:{args.model_path} |')
+    # 加载数据
+    start_time = time.time()
     # 加载数据
     df = pd.read_csv(args.data_path)
     input_data = np.array(df[args.input_column].astype(np.float32))[-1000 + args.output_size:]  # 限定长度方便画图
     output_data = np.array(df[args.output_column].astype(np.float32))[-1000 + args.output_size:]  # 限定长度方便画图
+    input_data = input_data.astype(np.float16 if args.float16 else np.float32)
+    input_len = len(input_data) - args.input_size + 1
+    input_batch = [input_data[_:_ + args.input_size] for _ in range(input_len)]
+    input_batch = np.stack(input_batch, axis=0).transpose(0, 2, 1)
+    end_time = time.time()
+    print('| 数据加载成功:{} 耗时:{:.4f} |'.format(args.data_path, end_time - start_time))
     # 推理
     start_time = time.time()
     middle = args.output_size // 2
     last = args.output_size
     result_middle = []
     result_last = []
-    dataloader = torch.utils.data.DataLoader(torch_dataset(args, input_data), batch_size=args.batch,
-                                             shuffle=False, drop_last=False, pin_memory=False,
-                                             num_workers=args.num_worker)
-    with torch.no_grad():
-        for item, batch in enumerate(dataloader):
-            batch = batch.to(args.device)
-            pred_batch = model(batch).detach().cpu().numpy()
+    n = input_len // args.batch
+    if n > 0:  # 如果预测数量>=批量(分批预测)
+        for i in range(n):
+            batch = input_batch[i * args.batch:(i + 1) * args.batch]
+            pred_batch = session.run([output_name], {input_name: batch})[0]
             result_middle.append(pred_batch[:, :, middle - 1])
             result_last.append(pred_batch[:, :, last - 1])
-        result_middle = np.concatenate(result_middle, axis=0)
-        result_last = np.concatenate(result_last, axis=0)
+        if input_len % args.batch > 0:  # 如果图片数量没有刚好满足批量
+            batch = input_batch[(i + 1) * args.batch:]
+            pred_batch = session.run([output_name], {input_name: batch})[0]
+            result_middle.append(pred_batch[:, :, middle - 1])
+            result_last.append(pred_batch[:, :, last - 1])
+    else:  # 如果图片数量<批量(直接预测)
+        batch = input_batch
+        pred_batch = session.run([output_name], {input_name: batch})[0]
+        result_middle.append(pred_batch[:, :, middle - 1])
+        result_last.append(pred_batch[:, :, last - 1])
+    result_middle = np.concatenate(result_middle, axis=0)
+    result_last = np.concatenate(result_last, axis=0)
     end_time = time.time()
-    print('| 数据:{} 批量:{} 平均耗时:{:.4f} |'
-          .format(args.data_path, args.batch, (end_time - start_time) / len(result_middle)))
+    print('| 数据:{} 批量:{} 每次预测耗时:{:.4f} |'.format(input_len, args.batch, (end_time - start_time) / input_len))
     # 画图
     draw(result_middle, output_data, middle)
     draw(result_last, output_data, last)
     print(f'| 画图保存位置:{args.save_path} |')
 
 
-class torch_dataset(torch.utils.data.Dataset):
-    def __init__(self, args, input_data):
-        self.input_size = args.input_size
-        self.output_size = args.output_size
-        self.input_data = input_data
-
-    def __len__(self):
-        return len(self.input_data) - self.input_size + 1
-
-    def __getitem__(self, index):
-        boundary = index + self.input_size
-        series = self.input_data[index:boundary]  # 输入数据
-        series = torch.tensor(series, dtype=torch.float32).permute(1, 0)  # 转换为tensor
-        return series
-
-
 if __name__ == '__main__':
-    test_pt()
+    test_onnx()
