013324f1cd498f368f1d589c2314dff28807197d
twk <1024565378@qq.com>
2023-04-10 17:59:51 +0800

    !

--- run.py
+++ run.py
@@ -33,7 +33,7 @@ parser.add_argument('--batch', default=64, type=int, help='|训练批量大小|'
 parser.add_argument('--loss', default='mse', type=str, help='|损失函数|')
 parser.add_argument('--lr_start', default=0.001, type=float, help='|初始学习率，训练中采用adam算法|')
 parser.add_argument('--lr_end', default=0.00005, type=float, help='|最终学习率|')
-parser.add_argument('--lr_adjust_num', default=50, type=int, help='|从初始学习率到最终学习率经过的调整次数|')
+parser.add_argument('--lr_adjust_num', default=100, type=int, help='|从初始学习率到最终学习率经过的调整次数|')
 parser.add_argument('--device', default='cuda', type=str, help='|训练设备|')
 parser.add_argument('--latch', default=True, type=bool, help='|模型和数据是否为锁存，True为锁存|')
 parser.add_argument('--num_worker', default=0, type=int, help='|CPU在处理数据时使用的进程数，0表示只有一个主进程，一般为0、2、4、8|')
