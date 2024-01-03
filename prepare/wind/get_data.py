41e6d684f9895558f53b9a1d53c127538429035c
twk <1024565378@qq.com>
2024-01-03 23:45:36 +0800

    !

--- prepare/wind/get_data.py
+++ prepare/wind/get_data.py
@@ -5,34 +5,32 @@ import argparse
 import numpy as np
 import pandas as pd
 
+# -------------------------------------------------------------------------------------------------------------------- #
 # 股票数据使用前复权，只获取交易日数据
 # wind试用版调用一次函数大概只能获取2500个数据，多了会报错
 # 除此之外每日和每周都有总额限定，超了之后会报错，要等一段时间再试
 # 数据精度为float64
-# -------------------------------------------------------------------------------------------------------------------- #
-# 设置
-parser = argparse.ArgumentParser(description='|从WindPy获取最新股票数据以补全数据库|')
-parser.add_argument('--save_path', default='dataset', type=str, help='|数据保存的位置|')
-parser.add_argument('--number', default='number.yaml', type=str, help='|选用的股票|')
-parser.add_argument('--variable', default='variable.yaml', type=str, help='|选用的变量|')
-parser.add_argument('--start_time', default='2010-01-01', type=str, help='|开始时间|')
-parser.add_argument('--end_time', default='2023-09-1', type=str, help='|结束时间，不超过当前日期的前一天|')
-parser.add_argument('--max_data', default=2500, type=int, help='|wind函数每次的获取上限，用于将数据划分为多次获取|')
-args = parser.parse_args()
-# -------------------------------------------------------------------------------------------------------------------- #
-# 初步检查
-assert os.path.exists(args.save_path), f'| os.path.exists(args.save_path) = {os.path.exists(args.save_path)} |'
-print(f'| {args.start_time} --> {args.end_time} |')
-
-
+# 获取的数据保存为:股票名.csv。重复获取时，只会获取新的数据并补全原数据
 # -------------------------------------------------------------------------------------------------------------------- #
 # 测试
 # WindPy.w.start()  # 默认命令超时时间为120秒，如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒
 # assert WindPy.w.isconnected(), f'| WindPy.w.isconnected()={WindPy.w.isconnected()} |'
 # wind_data = WindPy.w.wsd('600000.SH', 'open', '2010-01-01', '2010-02-01', 'Currency=CNY;PriceAdj=F')
+# assert False
+# -------------------------------------------------------------------------------------------------------------------- #
+# 设置
+parser = argparse.ArgumentParser(description='|通过WindPy获取最新股票数据以补全数据库|')
+parser.add_argument('--save_path', default='dataset', type=str, help='|数据保存的目录|')
+parser.add_argument('--number', default='number.yaml', type=str, help='|选用的股票|')
+parser.add_argument('--variable', default='variable.yaml', type=str, help='|选用的变量|')
+parser.add_argument('--start_time', default='2010-01-01', type=str, help='|开始时间|')
+parser.add_argument('--end_time', default='2023-09-1', type=str, help='|结束时间，要小于当前日期的前一天|')
+parser.add_argument('--max_data', default=2500, type=int, help='|wind函数每次的获取上限，将数据分为多次获取|')
+args = parser.parse_args()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
+# 程序
 def read_yaml(path):  # 读取yaml文件
     with open(path, 'r', encoding='utf-8')as f:
         yaml_dict = yaml.load(f, Loader=yaml.SafeLoader)
@@ -108,5 +106,5 @@ def get_data(number_dict, variable_dict, start_time, end_time, save_path, divide
 if __name__ == '__main__':
     number_dict = read_yaml(args.number)  # 读取选用的股票号
     variable_dict = read_yaml(args.variable)  # 读取选用的变量
-    divide = int(args.max_data // (len(variable_dict) * 0.7))  # 计算函数每次能获取的最大日期数，0.7是因为约0.7的比例为交易日
+    divide = int(args.max_data // (len(variable_dict) * 0.7))  # 计算函数每次能获取的最大日期数，约0.7的比例为交易日
     get_data(number_dict, variable_dict, args.start_time, args.end_time, args.save_path, divide)
