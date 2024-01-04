c90c586019d2f145e429c67bbacefc43d8ec4ef6
twk <1024565378@qq.com>
2024-01-04 23:47:13 +0800

    !

--- block/train_get.py
+++ block/train_get.py
@@ -71,9 +71,9 @@ def train_get(args, data_dict, model_dict, loss):
         if args.local_rank == 0:
             tqdm_show.close()
         # 计算平均损失
-        train_loss = train_loss / (index + 1)
+        train_loss /= (index + 1)
         if args.local_rank == 0:
-            print('\n| train_loss:{:.4f} | lr:{:.6f} |\n'.format(train_loss, optimizer.param_groups[0]['lr']))
+            print(f'\n| train_loss:{train_loss:.4f} | lr:{optimizer.param_groups[0]["lr"]:.6f} |\n')
         # 调整学习率
         optimizer = optimizer_adjust(optimizer, epoch + 1, train_loss)
         # 清理显存空间
--- block/val_get.py
+++ block/val_get.py
@@ -8,14 +8,16 @@ def val_get(args, val_dataloader, model, loss, data_dict, ema):
         model = ema.ema if args.ema else model.eval()
         pred = []
         true = []
+        val_loss = 0
         tqdm_len = ((data_dict['val_input'].shape[1] - args.input_size - args.output_size + 1)
                     // args.batch // args.device_number * args.device_number)
         tqdm_show = tqdm.tqdm(total=tqdm_len, mininterval=0.2)
-        for item, (series_batch, true_batch) in enumerate(val_dataloader):
+        for index, (series_batch, true_batch) in enumerate(val_dataloader):
             series_batch = series_batch.to(args.device, non_blocking=args.latch)
             true_batch = true_batch.to(args.device, non_blocking=args.latch)
             pred_batch = model(series_batch)
             loss_batch = loss(pred_batch, true_batch)
+            val_loss += loss_batch.item()
             pred.extend(pred_batch.cpu())
             true.extend(true_batch.cpu())
             # tqdm
@@ -24,12 +26,12 @@ def val_get(args, val_dataloader, model, loss, data_dict, ema):
         # tqdm
         tqdm_show.close()
         # 计算指标
+        val_loss /= (index + 1)
         pred = torch.stack(pred, dim=0)
         true = torch.stack(true, dim=0)
-        val_loss = loss(pred, true)
         # 计算总相对指标
         mae, mse = metric(pred, true)
-        print('\n| all | val_loss:{:.4f} | val_mae:{:.4f} | val_mse:{:.4f} |'.format(val_loss, mae, mse))
+        print(f'\n| all | val_loss:{val_loss:.4f} | val_mae:{mae:.4f} | val_mse:{mse:.4f} |')
         # 计算各类别相对指标和真实指标
         for i in range(pred.shape[1]):
             column = args.output_column[i]
@@ -39,4 +41,4 @@ def val_get(args, val_dataloader, model, loss, data_dict, ema):
             _mae_true, _mse_true = metric(pred[:, i], true[:, i])
             print('| {} | mae:{:.4f} | mse:{:.4f} | mae_true:{:.4f} | mse_true:{:.4f} |'
                   .format(column, _mae, _mse, _mae_true, _mse_true))
-    return val_loss.item(), mae.item(), mse.item()
+    return val_loss, mae.item(), mse.item()
