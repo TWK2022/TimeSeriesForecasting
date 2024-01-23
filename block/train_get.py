afdbcf94c3b9236ea5707bf2eae37e9f740eb917
TWK <109967047+TWK2022@users.noreply.github.com>
2024-01-23 18:17:46 +0800

    !

--- block/train_get.py
+++ block/train_get.py
@@ -11,8 +11,10 @@ def train_get(args, data_dict, model_dict, loss):
     # 学习率
     optimizer = adam(args.regularization, args.r_value, model.parameters(), lr=args.lr_start, betas=(0.937, 0.999))
     optimizer.load_state_dict(model_dict['optimizer_state_dict']) if model_dict['optimizer_state_dict'] else None
-    optimizer_adjust = lr_adjust(args, model_dict['lr_adjust_index'])  # 学习率调整函数
-    optimizer = optimizer_adjust(optimizer, model_dict['epoch'] + 1, 0)  # 初始化学习率
+    step_epoch = ((data_dict['train_input'].shape[1] - args.input_size - args.output_size + 1)
+                  // args.batch // args.device_number * args.device_number)  # 每轮的步数
+    optimizer_adjust = lr_adjust(args, step_epoch, model_dict['epoch_finished'])  # 学习率调整函数
+    optimizer = optimizer_adjust(optimizer)  # 学习率初始化
     # 使用平均指数移动(EMA)调整参数(不能将ema放到args中，否则会导致模型保存出错)
     ema = ModelEMA(model) if args.ema else None
     if args.ema:
@@ -33,14 +35,13 @@ def train_get(args, data_dict, model_dict, loss):
     # 分布式初始化
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
                                                       output_device=args.local_rank) if args.distributed else model
-    epoch_base = model_dict['epoch'] + 1  # 新的一轮要+1
-    for epoch in range(epoch_base, epoch_base + args.epoch):  # 训练
+    epoch_base = model_dict['epoch_finished'] + 1  # 新的一轮要+1
+    for epoch in range(epoch_base, args.epoch):  # 训练
         print(f'\n-----------------------第{epoch}轮-----------------------') if args.local_rank == 0 else None
         model.train()
         train_loss = 0  # 记录损失
         if args.local_rank == 0:  # tqdm
-            tqdm_len = ((data_dict['train_input'].shape[1] - args.input_size - args.output_size + 1)
-                        // args.batch // args.device_number * args.device_number)
+            tqdm_len = step_epoch
             tqdm_show = tqdm.tqdm(total=tqdm_len, mininterval=0.2)
         for index, (series_batch, true_batch) in enumerate(train_dataloader):
             series_batch = series_batch.to(args.device, non_blocking=args.latch)
@@ -63,9 +64,12 @@ def train_get(args, data_dict, model_dict, loss):
             ema.update(model) if args.ema else None
             # 记录损失
             train_loss += loss_batch.item()
+            # 调整学习率
+            optimizer = optimizer_adjust(optimizer)
             # tqdm
             if args.local_rank == 0:
-                tqdm_show.set_postfix({'train_loss': loss_batch.item()})  # 添加loss显示
+                tqdm_show.set_postfix({'train_loss': loss_batch.item(),
+                                       'lr': optimizer.param_groups[0]["lr"]})  # 添加显示
                 tqdm_show.update(args.device_number)  # 更新进度条
         # tqdm
         if args.local_rank == 0:
@@ -73,9 +77,7 @@ def train_get(args, data_dict, model_dict, loss):
         # 计算平均损失
         train_loss /= index + 1
         if args.local_rank == 0:
-            print(f'\n| train_loss:{train_loss:.4f} | lr:{optimizer.param_groups[0]["lr"]:.6f} |\n')
-        # 调整学习率
-        optimizer = optimizer_adjust(optimizer, epoch + 1, train_loss)
+            print(f'\n| 训练 | train_loss:{train_loss:.4f} | lr_now:{optimizer.param_groups[0]["lr"]:.6f} |\n')
         # 清理显存空间
         del series_batch, true_batch, pred_batch, loss_batch
         torch.cuda.empty_cache()
@@ -85,9 +87,8 @@ def train_get(args, data_dict, model_dict, loss):
         # 保存
         if args.local_rank == 0:  # 分布式时只保存一次
             model_dict['model'] = model.module if args.distributed else model
-            model_dict['epoch'] = epoch
+            model_dict['epoch_finished'] = epoch
             model_dict['optimizer_state_dict'] = optimizer.state_dict()
-            model_dict['lr_adjust_index'] = optimizer_adjust.lr_adjust_index
             model_dict['ema_updates'] = ema.updates if args.ema else model_dict['ema_updates']
             model_dict['mean_input'] = data_dict['mean_input']
             model_dict['mean_output'] = data_dict['mean_output']
@@ -98,7 +99,7 @@ def train_get(args, data_dict, model_dict, loss):
             model_dict['val_mae'] = mae
             model_dict['val_mse'] = mse
             torch.save(model_dict, 'last.pt')  # 保存最后一次训练的模型
-            if epoch > args.epoch // 4 and mse < 1 and mse < model_dict['standard']:
+            if mse < 1 and mse < model_dict['standard']:
                 model_dict['standard'] = mse
                 torch.save(model_dict, args.save_path)  # 保存最佳模型
                 print(f'\n| 保存最佳模型:{args.save_path} | val_loss:{val_loss:.4f} | val_mae:{mae:.4f} |'
