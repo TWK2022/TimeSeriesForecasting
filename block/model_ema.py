4278bd7b75db08cab42cff3d35c679ff2946d79c
TWK <109967047+TWK2022@users.noreply.github.com>
2024-02-02 16:37:55 +0800

    !

--- block/model_ema.py
+++ block/model_ema.py
@@ -1,35 +1,27 @@
 import math
+import copy
 import torch
-from copy import deepcopy
 
 
 class model_ema:
-    """ Updated Exponential Moving Average (EMA) from https://github.com/rwightman/pytorch-image-models
-    Keeps a moving average of everything in the model state_dict (parameters and buffers)
-    For EMA details see https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
-    """
-
     def __init__(self, model, decay=0.9999, tau=2000, updates=0):
-        # Create EMA
-        self.ema = deepcopy(self.de_parallel(model)).eval()  # FP32 EMA
-        # if next(model.parameters()).device.type != 'cpu':
-        self.updates = updates  # number of EMA updates
-        self.decay = lambda x: decay * (1 - math.exp(-x / tau))  # decay exponential ramp (to help early epochs)
+        self.ema = copy.deepcopy(self._get_model(model)).eval()  # FP32 EMA
+        self.updates = updates
+        self.decay = lambda x: decay * (1 - math.exp(-x / tau))
         for p in self.ema.parameters():
             p.requires_grad_(False)
 
     def update(self, model):
-        # Update EMA parameters
         with torch.no_grad():
             self.updates += 1
             d = self.decay(self.updates)
-            msd = self.de_parallel(model).state_dict()  # model state_dict
+            state_dict = self._get_model(model).state_dict()
             for k, v in self.ema.state_dict().items():
                 if v.dtype.is_floating_point:
                     v *= d
-                    v += (1 - d) * msd[k].detach()
+                    v += (1 - d) * state_dict[k].detach()
 
-    def de_parallel(self, model):
+    def _get_model(self, model):
         if type(model) in (torch.nn.parallel.DataParallel, torch.nn.parallel.DistributedDataParallel):
             return model.module
         else:
