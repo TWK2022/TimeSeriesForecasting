d507433ea570118d8c03e344d1b2928a0283edf1
twk <1024565378@qq.com>
2024-03-13 23:02:44 +0800

    !

--- prepare/tushare/get_data.py
+++ prepare/tushare/get_data.py
@@ -12,4 +12,4 @@ args = parser.parse_args()
 if __name__ == '__main__':
     tushare.set_token(args.token)  # 设置密钥
     pro = tushare.pro_api()  # 初始化接口
-    df = pro.daily(ts_code='000001.SZ', start_date='20140101', end_date='20240201')
+    df = pro.daily(ts_code='000001.SZ', start_date='20240101', end_date='20240201')
