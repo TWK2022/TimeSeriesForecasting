9ad7e122c92bfd4aa073d0972d272c0fbb4c675f
TWK <109967047+TWK2022@users.noreply.github.com>
2024-03-20 14:42:12 +0800

    !

--- block/data_get.py
+++ block/data_get.py
@@ -23,26 +23,32 @@ class data_prepare:
             df = pd.read_csv(self.data_path, encoding='utf-8')
         except:
             df = pd.read_csv(self.data_path, encoding='gbk')
-        input_data = np.array(df[self.input_column].astype(np.float32)).transpose(1, 0)
-        output_data = np.array(df[self.output_column].astype(np.float32)).transpose(1, 0)
+        input_data = np.array(df[self.input_column]).astype(np.float32)
+        output_data = np.array(df[self.output_column]).astype(np.float32)
         # 划分数据集
         add = self.input_size + self.output_size - 1  # 输入数据后面的补足
         data_len = len(df) - add  # 输入数据的真实数量
         boundary = int(data_len * self.divide[0] / (self.divide[0] + self.divide[1]))  # 数据划分
-        train_input = input_data[:, 0:boundary + add]  # 训练数据
-        train_output = output_data[:, 0:boundary + add]  # 训练标签
-        val_input = input_data[:, boundary:len(df)].copy()  # 验证数据
-        val_output = output_data[:, boundary:len(df)].copy()  # 验证标签
+        train_input = input_data[0:boundary + add]  # 训练数据
+        train_output = output_data[0:boundary + add]  # 训练标签
+        val_input = input_data[boundary:len(df)].copy()  # 验证数据
+        val_output = output_data[boundary:len(df)].copy()  # 验证标签
         # 周期
         if self.z_score_cycle == -1:
-            self.max_cycle = train_input.shape[1]
+            max_cycle = input_data.shape[0]
         else:
-            assert self.z_score_cycle <= train_input.shape[1], f'! 周期设置不能大于训练集长度 !'
-            self.max_cycle = train_input.shape[1] // self.z_score_cycle * self.z_score_cycle
-        # 数据处理
-        train_input, val_input, mean_input, std_input = self._z_score(train_input, val_input, self.input_column)
-        train_output, val_output, mean_output, std_output = self._z_score(train_output, val_output, self.output_column)
-        # 将所有数据存放到一个大字典中
+            assert self.z_score_cycle <= input_data.shape[0], f'! 周期设置不能大于训练集长度 !'
+            max_cycle = input_data.shape[0] // self.z_score_cycle * self.z_score_cycle
+        # 归一化
+        mean_input = np.mean(input_data[0:max_cycle], axis=0)
+        mean_output = np.mean(output_data[0:max_cycle], axis=0)
+        std_input = np.std(input_data[0:max_cycle], axis=0)
+        std_output = np.std(output_data[0:max_cycle], axis=0)
+        train_input = (train_input - mean_input) / std_input
+        val_input = (val_input - mean_input) / std_input
+        train_output = (train_output - mean_output) / std_output
+        val_output = (val_output - mean_output) / std_output
+        # 记录数据
         data_dict = {}
         data_dict['train_input'] = train_input
         data_dict['train_output'] = train_output
@@ -53,16 +59,3 @@ class data_prepare:
         data_dict['std_input'] = std_input
         data_dict['std_output'] = std_output
         return data_dict
-
-    def _z_score(self, train_data, val_data, column):  # 减均值、除以方差
-        mean_all = np.zeros(len(column))
-        std_all = np.zeros(len(column))
-        for i in range(len(column)):
-            mean = np.mean(train_data[i, 0:self.max_cycle])
-            std = np.std(train_data[i, 0:self.max_cycle])
-            std = max(std, 0.01)
-            mean_all[i] = mean
-            std_all[i] = std
-            train_data[i, :] = (train_data[i, :] - mean) / std
-            val_data[i, :] = (val_data[i, :] - mean) / std
-        return train_data, val_data, mean_all, std_all
