f1da2db5ac5b8aaff71d849800e784802cb84ab2
TWK <109967047+TWK2022@users.noreply.github.com>
2024-03-28 20:22:32 +0800

    !

--- model/linear_conv.py
+++ model/linear_conv.py
@@ -1,6 +1,6 @@
 # 多变量异标签
 import torch
-from model.layer import cbs, split_linear
+from model.layer import split_linear, llg, clg
 
 
 class linear_conv(torch.nn.Module):
@@ -11,22 +11,22 @@ class linear_conv(torch.nn.Module):
         input_size = args.input_size
         output_size = args.output_size
         # 网络结构
-        self.split_linear0 = split_linear(input_dim, input_size)
-        self.linear1 = torch.nn.Linear(input_size, input_size, bias=True)
-        self.linear2 = torch.nn.Linear(input_size, output_size, bias=True)
-        self.cbs3 = cbs(input_dim, 16 * input_dim, 1, 1)  # 推荐'4至16 * input_dim'
-        self.conv4 = torch.nn.Conv1d(16 * input_dim, output_dim, kernel_size=1, stride=1)
+        self.l0 = split_linear(input_dim, input_size)
+        self.l1 = llg(input_size, input_size)
+        self.l2 = llg(input_size, output_size)
+        self.l3 = clg(input_dim, 8 * input_dim, output_size, 1, 1)
+        self.l4 = torch.nn.Conv1d(8 * input_dim, output_dim, kernel_size=1, stride=1)
 
     def forward(self, x):
         # 输入(batch,input_dim,input_size)
         series_last = x[:, :, -1:]
         x = x - series_last
-        x = self.split_linear0(x)
-        x = self.linear1(x)
-        x = self.linear2(x)
+        x = self.l0(x)
+        x = self.l1(x)
+        x = self.l2(x)
         x = x + series_last
-        x = self.cbs3(x)
-        x = self.conv4(x)
+        x = self.l3(x)
+        x = self.l4(x)
         return x
 
 
@@ -36,8 +36,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_column', default='1,2,3', type=str)
     parser.add_argument('--output_column', default='1', type=str)
-    parser.add_argument('--input_size', default=128, type=int)
-    parser.add_argument('--output_size', default=64, type=int)
+    parser.add_argument('--input_size', default=512, type=int)
+    parser.add_argument('--output_size', default=256, type=int)
     args = parser.parse_args()
     args.input_column = args.input_column.split(',')
     args.output_column = args.output_column.split(',')
