47657a855610915eb18abb47f23ad952a321dfd9
TWK <109967047+TWK2022@users.noreply.github.com>
2024-04-01 17:27:04 +0800

    !

--- block/loss_get.py
+++ block/loss_get.py
@@ -9,15 +9,16 @@ def loss_get(args):
     return loss
 
 
-class mse_decay:  # 使越靠近前面的数的准确率越重要
+class mse_decay:  # 使越靠近前面的数值的准确率越重要
     def __init__(self, args):
-        start = 1.5
-        end = 0.5
-        self.decay = torch.arange(start, end, -1 / args.output_size).to(args.device)
+        start = 1.3
+        end = 0.7
+        self.decay = torch.arange(start, end, -1 / args.output_size)
         self.mse = torch.nn.MSELoss()
 
     def __call__(self, pred, true):
-        pred_decay = pred * self.decay
-        true_decay = true * self.decay
+        device = pred.device
+        pred_decay = pred * self.decay.to(device)
+        true_decay = true * self.decay.to(device)
         loss = self.mse(pred_decay, true_decay)
         return loss
