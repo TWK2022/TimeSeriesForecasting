970ca7698ea06c79415bdbb3a5114b69a5d70570
TWK <109967047+TWK2022@users.noreply.github.com>
2024-04-08 22:45:56 +0800

    !

--- prepare/tushare/get_data.py
+++ prepare/tushare/get_data.py
@@ -8,12 +8,11 @@ import pandas as pd
 # 获取数据的时间格式为20240101，不能为2024-01-01
 # -------------------------------------------------------------------------------------------------------------------- #
 parser = argparse.ArgumentParser(description='|通过tushare获取最新股票数据以补全数据库|')
-parser.add_argument('--token', default='', type=str,
-                    help='|密钥|')
+parser.add_argument('--token', default='', type=str, help='|密钥|')
 parser.add_argument('--save_path', default='dataset', type=str, help='|数据保存的目录|')
 parser.add_argument('--number', default='number.yaml', type=str, help='|选用的股票|')
 parser.add_argument('--start_time', default='20150101', type=str, help='|开始时间|')
-parser.add_argument('--end_time', default='20240410', type=str, help='|结束时间|')
+parser.add_argument('--end_time', default='20240420', type=str, help='|结束时间|')
 args = parser.parse_args()
 
 
