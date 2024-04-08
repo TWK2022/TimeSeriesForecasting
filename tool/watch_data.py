abebf494d8ee78e6a5b317185ef15b0f38067d00
TWK <109967047+TWK2022@users.noreply.github.com>
2024-04-08 22:26:25 +0800

    !

--- tool/watch_data.py
+++ tool/watch_data.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 # 设置
 parser = argparse.ArgumentParser(description='|查看数据|')
 parser.add_argument('--data_path', default=r'../dataset/ETTh1.csv', type=str, help='|数据路径|')
+parser.add_argument('--column', default='HUFL,HULL,MUFL,MULL,LUFL,LULL,OT', type=str, help='|数据路径|')
 parser.add_argument('--show_len', default=2000, type=int, help='|显示的数据长度|')
 args = parser.parse_args()
+args.column = args.column.split(',')
 # -------------------------------------------------------------------------------------------------------------------- #
 # 程序
 if __name__ == '__main__':
@@ -18,7 +20,7 @@ if __name__ == '__main__':
     plt.title(name)
     plt.xlabel('time')
     plt.ylabel('value')
-    for column in df.columns[1:]:
+    for column in args.column:
         y = df[column][-args.show_len:].values
         plt.plot(y, label=column)
     plt.legend()
