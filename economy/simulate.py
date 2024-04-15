4373198445f8a9ac7cbfe16c91e25b6361267059
TWK <109967047+TWK2022@users.noreply.github.com>
2024-04-15 11:45:58 +0800

    !

--- economy/simulate.py
+++ economy/simulate.py
@@ -21,9 +21,9 @@ parser.add_argument('--input_size', default=96, type=int, help='|输入长度|')
 parser.add_argument('--output_size', default=24, type=int, help='|输出长度|')
 parser.add_argument('--divide', default='19,1', type=str, help='|训练集和验证集划分比例，取验证集测试|')
 parser.add_argument('--rise', default=1.3, type=float, help='|上涨预期，大于预期才会买入，数值越大越保险，基准为1.3|')
-parser.add_argument('--decline', default=0.95, type=float, help='|下跌预期，小于预期才会卖出，基准为0.95|')
 parser.add_argument('--a_rise_max', default=1.3, type=float, help='|达到预期收益，直接卖出，数值太大时无效|')
 parser.add_argument('--a_rise_still', default=1.05, type=float, help='|第2天发现还在上涨，先不卖出，数值太大时无效|')
+parser.add_argument('--a_decline_still', default=0.97, type=float, help='|第2天发现还在下降，先不买入，数值太小时无效|')
 parser.add_argument('--device', default='cpu', type=str, help='|推理设备|')
 args = parser.parse_args()
 args.divide = list(map(int, args.divide.split(',')))
@@ -38,9 +38,9 @@ assert os.path.exists(args.data_path), f'! data_path不存在:{args.data_path} !
 class project_class:
     def __init__(self, args):
         self.rise = args.rise
-        self.decline = args.decline
         self.a_rise_max = args.a_rise_max
         self.a_rise_still = args.a_rise_still
+        self.a_decline_still = args.a_decline_still
         self.input_size = args.input_size
         self.output_size = args.output_size
         divide = args.divide
@@ -78,7 +78,7 @@ class project_class:
             self.state = 0
             self.buy_list = []
             self.sell_list = []
-            for index in range(self.input_size, self.input_data.shape[1] - self.output_size):  # index是预测的第一步
+            for index in range(self.input_size, self.input_data.shape[1] - 1):  # index是预测的第一步
                 tensor = torch.tensor(self.input_data[:, index - self.input_size:index]).unsqueeze(0)
                 pred = self.model(tensor)[0][0].cpu().numpy()
                 now = self.output_data[0, index - 1]
@@ -87,7 +87,7 @@ class project_class:
                 min_ = np.min(pred[3:-3])
                 if max_ > self.rise * now:  # 预测上涨
                     self._rise(now, next_, pred)
-                elif min_ < self.decline * now:  # 预测下降
+                elif min_ < now:  # 预测下降
                     self._decline(now, next_, pred)
                 else:  # 预测小幅波动
                     pass
@@ -97,15 +97,15 @@ class project_class:
         self.state = 0
         self.buy_list = []
         self.sell_list = []
-        for index in range(self.input_size, self.input_data.shape[1] - self.output_size):  # index是预测的第一步
-            pred = self.output_data[0, index:index + self.output_size]
+        for index in range(self.input_size, self.input_data.shape[1] - 10):  # index是预测的第一步
+            pred = self.output_data[0, index:min(index + self.output_size, self.input_data.shape[1])]
             now = self.output_data[0, index - 1]
             next_ = self.output_data[0, index]
             max_ = np.max(pred[3:-3])
             min_ = np.min(pred[3:-3])
             if max_ > self.rise * now:  # 预测上涨
                 self._rise(now, next_, pred)
-            elif min_ < self.decline * now:  # 预测下降
+            elif min_ < now:  # 预测下降
                 self._decline(now, next_, pred)
             else:  # 预测小幅波动
                 pass
@@ -115,7 +115,8 @@ class project_class:
         if self.state == 1:  # 已经买入
             return
         # a人为策略
-        pass
+        if now * self.a_decline_still > next_:  # 第2天发现依然有下降趋势，先不买入
+            return
         # b模型策略
         if now > np.mean(pred[0:3]):  # 预测还有下降空间，先不买入
             return
@@ -131,7 +132,7 @@ class project_class:
             self.state = 0
             self.sell_list.append(now + next_ / 2)
             return
-        if now < self.a_rise_still * next_:  # 第2天发现依然有上涨趋势，先不卖出
+        if now * self.a_rise_still < next_:  # 第2天发现依然有上涨趋势，先不卖出
             return
         # b模型策略
         if now < np.mean(pred[0:3]):  # 预测还有上升空间，先不卖出
