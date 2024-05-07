2e34576240130073e0a6c0056cad745c3d75a7b4
TWK <109967047+TWK2022@users.noreply.github.com>
2024-05-07 20:42:47 +0800

    !

--- economy/data_screen.py
+++ economy/data_screen.py
@@ -7,11 +7,12 @@ import pandas as pd
 parser = argparse.ArgumentParser(description='|筛选有上升潜力的股票|')
 parser.add_argument('--data_dir', default='dataset', type=str, help='|数据位置|')
 parser.add_argument('--save_path', default='data_screen.txt', type=str, help='|筛选结果保存位置|')
+parser.add_argument('--threshold', default=0.9, type=float, help='|筛选指标小于threshold的股票，1表示<=历史加权均值|')
 args = parser.parse_args()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-def data_screen(data_dir, save_path):
+def data_screen(data_dir, save_path, threshold):
     # 文件位置
     file_list = os.listdir(data_dir)
     screen_list = []
@@ -30,7 +31,7 @@ def data_screen(data_dir, save_path):
         ratio = 0.5 + np.arange(len(close_data)) / (len(close_data) - 1)
         mean = np.mean(close_data * ratio)
         close_metric = np.mean(close_data[-3:]) / mean
-        if close_metric > 1:
+        if close_metric > threshold:
             continue
         # 记录
         result_list.append(file_list[index].split('.')[0] + f' {close_metric:.2f}\n')
@@ -46,4 +47,4 @@ def data_screen(data_dir, save_path):
 
 # -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
-    data_screen(args.data_dir, args.save_path)
+    data_screen(args.data_dir, args.save_path, args.threshold)
