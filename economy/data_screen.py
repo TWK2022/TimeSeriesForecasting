aeb682a3a255d1f3592c8f7db81b0e051221632d
TWK <109967047+TWK2022@users.noreply.github.com>
2024-05-07 09:15:34 +0800

    !

--- economy/data_screen.py
+++ economy/data_screen.py
@@ -1,46 +1,46 @@
+import os
 import argparse
 import numpy as np
 import pandas as pd
 
 # -------------------------------------------------------------------------------------------------------------------- #
-parser = argparse.ArgumentParser(description='|计算5日、10日、60日均线|')
-parser.add_argument('--data_path', default='dataset/华银电力.csv', type=str, help='|数据位置|')
-parser.add_argument('--column', default='收盘价,成交量', type=str, help='|变量选择|')
+parser = argparse.ArgumentParser(description='|筛选有上升潜力的股票|')
+parser.add_argument('--data_dir', default='dataset', type=str, help='|数据位置|')
+parser.add_argument('--save_path', default='data_screen.txt', type=str, help='|筛选结果保存位置|')
 args = parser.parse_args()
-args.column = args.column.split(',')
-args.save_path = args.data_path.split('.')[0] + '_add.csv'
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-def count(data, lengh, column):
+def data_screen(data_dir, save_path):
+    # 文件位置
+    file_list = os.listdir(data_dir)
+    screen_list = []
+    for path in file_list:
+        if '_add' not in path:  # 原始数据
+            screen_list.append(path)
+    file_list = screen_list
+    path_list = [f'{data_dir}/{_}' for _ in file_list]
+    # 筛选
     result_list = []
-    for index in range(len(data) - lengh + 1):
-        result_list.append(np.sum(data[index:index + lengh, :], axis=0) / lengh)
-    result = np.stack(result_list, axis=0)
-    column = [f'{_}_{lengh}' for _ in column]
-    return result, column
-
-
-def data_add(data_path, column, save_path):
-    df = pd.read_csv(data_path, index_col=0)
-    value = df[column].values
-    result_5, column_5 = count(data=value, lengh=5, column=column)
-    result_10, column_10 = count(data=value, lengh=10, column=column)
-    result_60, column_60 = count(data=value, lengh=60, column=column)
-    lengh = len(result_60)
-    result_5 = result_5[-lengh:]
-    result_10 = result_10[-lengh:]
-    result = np.concatenate([result_5, result_10, result_60], axis=1)
-    column_add = column_5 + column_10 + column_60
-    index = df.index[-lengh:]
-    df_add = pd.DataFrame(result, columns=column_add, index=index)
-    drop_index = df.index[:-lengh]
-    df = df.drop(index=drop_index)
-    df = pd.concat([df, df_add], axis=1)
-    df.to_csv(save_path, header=True, index=True)
+    for index, path in enumerate(path_list):
+        df = pd.read_csv(path, index_col=0)
+        # 收盘价筛选
+        close_data = df['收盘价'].values
+        ratio = 0.5 + np.arange(len(close_data)) / (len(close_data) - 1)
+        mean = np.mean(close_data * ratio)
+        close_metric = np.mean(close_data[-3:]) / mean
+        if close_metric > 1:
+            continue
+        # 记录
+        result_list.append(file_list[index].split('.')[0] + f' {close_metric:.2f}\n')
+    # 保存
+    with open(save_path, 'w', encoding='utf-8') as f:
+        f.writelines(result_list)
+    # 显示
+    print(f'| 总数:{len(path_list)} | 筛选数:{len(result_list)} |')
     print(f'| 结果保存至:{save_path} |')
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
-    data_add(args.data_path, args.column, args.save_path)
+    data_screen(args.data_dir, args.save_path)
