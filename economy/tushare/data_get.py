45834c0240a948d7c5ef81f3f919ec5d20900b3d
TWK <109967047+TWK2022@users.noreply.github.com>
2024-05-07 09:52:23 +0800

    !

--- economy/tushare/data_get.py
+++ economy/tushare/data_get.py
@@ -20,16 +20,17 @@ if not os.path.exists(args.save_path):
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-class get_data_class:
+class data_get_class:
     def __init__(self, yaml_path):
-        with open(yaml_path, 'r', encoding='utf-8')as f:
+        with open(yaml_path, 'r', encoding='utf-8') as f:
             self.number_dict = yaml.load(f, Loader=yaml.SafeLoader)
         self.daily_column = ['trade_date', 'open', 'high', 'low', 'close', 'pre_close', 'change', 'pct_chg', 'vol']
-        self.daily_name = ['日期', '开盘价', '最高价', '最低价', '收盘价', '昨收价(前复权)', '涨跌额', '涨跌幅', '成交量']
+        self.daily_name = ['日期', '开盘价', '最高价', '最低价', '收盘价', '昨收价(前复权)', '涨跌额', '涨跌幅',
+                           '成交量']
         self.daily_basic_column = 'trade_date,turnover_rate,turnover_rate_f,volume_ratio,pe_ttm,pb,ps_ttm'
         self.daily_basic_name = ['日期', '换手率', '换手率(自由流通股)', '量比', '市盈率(ttm)', '市净率', '市销率(ttm)']
 
-    def get_data(self):
+    def data_get(self):
         tushare.set_token(args.token)  # 设置密钥
         pro = tushare.pro_api()  # 初始化接口
         for key in self.number_dict:
@@ -73,6 +74,7 @@ class get_data_class:
         return df
 
 
+# -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
-    model = get_data_class(args.number)
-    model.get_data()
+    model = data_get_class(args.number)
+    model.data_get()
