32b928a0a3e0748ed4386ce2b741bfe033210cb5
TWK <109967047+TWK2022@users.noreply.github.com>
2024-05-08 17:03:39 +0800

    !

--- block/util.py
+++ block/util.py
@@ -5,7 +5,6 @@ def read_column(column_file):  # column处理
     if os.path.exists(column_file):
         with open(column_file, encoding='utf-8') as f:
             column = [_.strip() for _ in f.readlines()]
-        print(f'| 读取column文件:{column_file} |')
     else:
         column = column_file.split(',')
     return column
