e9ab2cf9634d94d7c5d5ebe529c8708645b9e931
TWK <109967047+TWK2022@users.noreply.github.com>
2024-05-14 16:41:27 +0800

    !

--- model/crossformer.py
+++ model/crossformer.py
@@ -124,10 +124,10 @@ class crossformer(torch.nn.Module):
         output_dim = len(args.output_column)
         input_size = args.input_size
         output_size = args.output_size
-        n_dict = {'s': 32, 'm': 64, 'l': 128}
+        n_dict = {'s': 16, 'm': 32, 'l': 64}
         feature = n_dict[args.model_type]
         head = 8
-        middle_dim = 10
+        middle_dim = 5
         self.segment = output_size // 4
         assert input_size % (self.segment * 4) == 0 or output_size % (self.segment * 2) == 0
         input_number = input_size // self.segment
