6d4b00b3007fe4dc2276508aef3808c7c3760e77
TWK <109967047+TWK2022@users.noreply.github.com>
2024-05-16 16:53:28 +0800

    !

--- block/metric_get.py
+++ block/metric_get.py
@@ -3,5 +3,5 @@ import torch
 
 def metric(pred, true):
     mae = torch.mean(abs(pred - true))
-    mse = torch.mean(torch.square(pred - true))
-    return mae, mse
+    rmse = torch.sqrt(torch.mean(torch.square(pred - true)))
+    return mae, rmse
