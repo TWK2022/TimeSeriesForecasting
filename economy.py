088c15b43decd8f5ab8bb86552ac6fdc24d9875d
twk <1024565378@qq.com>
2024-06-06 21:12:15 +0800

    !

--- economy.py
+++ economy.py
@@ -20,7 +20,7 @@ parser.add_argument('--model', default='special_add', type=str)
 parser.add_argument('--model_type', default='l', type=str)
 # economy/tushare/industry_choice.py
 parser.add_argument('--industry_choice', default=False, type=bool)
-parser.add_argument('--industry', default='电气设备,运输设备,小金属,黄金,铝,铜,铅锌', type=str)
+parser.add_argument('--industry', default='电气设备,运输设备,工程机械,医疗保健,小金属,黄金,铝,铜,铅锌', type=str)
 # economy/tushare/data_get.py
 parser.add_argument('--data_get', default=False, type=bool)
 parser.add_argument('--token', default='', type=str)
