e37127832d3a1faa73faebeab3c75c50628759cd
TWK <109967047+TWK2022@users.noreply.github.com>
2024-06-11 10:02:25 +0800

    !

--- economy.py
+++ economy.py
@@ -276,7 +276,7 @@ class economy_class:
                     else:
                         pred = model(tensor)[0][0].cpu().numpy()
                 # 画图
-                ratio = np.max(pred) / close_data[-1]
+                ratio = np.max(pred[0:3]) / close_data[-1]
                 if ratio > self.args.draw_threshold or industry == '自选':  # 有上涨空间或自选股票
                     last_day = str(df.index[-1])
                     mean_judge = self._count(df['收盘价_5'].values, df['收盘价_10'].values)
