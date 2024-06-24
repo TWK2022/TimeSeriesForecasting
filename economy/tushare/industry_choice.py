b1103e5f7f5f859f46a6dd1db156b4b67ef5f022
TWK <109967047+TWK2022@users.noreply.github.com>
2024-06-24 11:04:29 +0800

    !

--- economy/tushare/industry_choice.py
+++ economy/tushare/industry_choice.py
@@ -7,9 +7,10 @@ import argparse
 # -------------------------------------------------------------------------------------------------------------------- #
 parser = argparse.ArgumentParser(description='|选择股票|')
 parser.add_argument('--yaml_path', default='number_all.yaml', type=str, help='|所有股票信息|')
-parser.add_argument('--other_path', default='other.yaml', type=str, help='|自选股票信息(可选)|')
+parser.add_argument('--reserve_path', default='reserve.yaml', type=str, help='|自选股票信息(可选)|')
+parser.add_argument('--remove_path', default='remove.yaml', type=str, help='|要去除的股票信息(可选)|')
 parser.add_argument('--save_path', default='number.yaml', type=str, help='|保存位置|')
-parser.add_argument('--industry', default='电气设备,运输设备,通信设备,元器件,小金属,黄金,铝,铜,铅锌', type=str, help='|行业，必选，如"A,B,C"|')
+parser.add_argument('--industry', default='电气设备,通信设备,元器件,小金属,黄金,铝,铜,铅锌', type=str, help='|行业，必选，如"A,B,C"|')
 parser.add_argument('--area', default='', type=str, help='|地区，空则不筛选，如"A、B、C"|')
 parser.add_argument('--time', default='20240101', type=str, help='|上市时间，筛选time之前的，空则不筛选|')
 parser.add_argument('--type', default='', type=str, help='|企业类型，有其他、中央国企、地方国企，空则不筛选|')
@@ -24,14 +25,21 @@ def industry_choice(args):
         yaml_dict = yaml.load(f, Loader=yaml.SafeLoader)
     result_dict = {}
     record = 0
-    other_list = []
+    reserve_list = []
+    remove_list = []
     # 自选股票
-    if os.path.exists(args.other_path):
-        with open(args.other_path, 'r', encoding='utf-8') as f:
-            other_dict = yaml.load(f, Loader=yaml.SafeLoader)
-        result_dict['自选'] = other_dict['自选']
-        record += len(other_dict['自选'])
-        other_list = other_dict['自选'].keys()
+    if os.path.exists(args.reserve_path):
+        with open(args.reserve_path, 'r', encoding='utf-8') as f:
+            reserve_dict = yaml.load(f, Loader=yaml.SafeLoader)
+        if reserve_dict is not None or result_dict['自选'] is not None:
+            result_dict['自选'] = reserve_dict['自选']
+            record += len(reserve_dict['自选'])
+            reserve_list = reserve_dict['自选'].keys()
+    if os.path.exists(args.remove_path):
+        with open(args.remove_path, 'r', encoding='utf-8') as f:
+            remove_dict = yaml.load(f, Loader=yaml.SafeLoader)
+        if remove_dict is not None or result_dict['去除'] is not None:
+            remove_list = remove_dict['去除'].keys()
     # 行业选择
     for industry in args.industry:
         result_dict[industry] = {}
@@ -40,7 +48,9 @@ def industry_choice(args):
             number = dict_[name][0]
             type_ = dict_[name][2]
             time = dict_[name][3]
-            if name in other_list:
+            if name in reserve_list:
+                continue
+            if name in remove_list:
                 continue
             if 'ST' in name:
                 continue
