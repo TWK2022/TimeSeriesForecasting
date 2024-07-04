66b7390927135ee110a712dd5e7628d0117604b4
TWK <109967047+TWK2022@users.noreply.github.com>
2024-07-04 16:48:28 +0800

    !

--- model/special_add.py
+++ model/special_add.py
@@ -1,28 +1,19 @@
 # 特殊模型
-# 多变量单标签
 import torch
-from model.layer import lgl
 from model.itransformer import itransformer
 
 
 class special_add(torch.nn.Module):
     def __init__(self, args, model=None):
         super().__init__()
-        output_size = args.output_size
         # 网络结构
         self.model = model(args) if model else itransformer(args)
-        self.lgl0 = lgl(output_size)
-        self.lgl1 = lgl(output_size)
 
     def forward(self, x, special=None):  # (batch,input_dim,input_size) -> (batch,output_dim,output_size)
+        series_last = torch.mean(x[:, [0, 1], -3:], dim=2, keepdim=True)
         x = self.model(x)
-        if special is None:
-            return x
-        else:
-            special = special.unsqueeze(1).to(x.device)
-            special = self.lgl0(x - special)
-            x = self.lgl1(x + special)
-            return x
+        x = x + series_last
+        return x
 
 
 if __name__ == '__main__':
@@ -30,7 +21,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_column', default='1,2,3', type=str)
-    parser.add_argument('--output_column', default='1', type=str)
+    parser.add_argument('--output_column', default='1,2', type=str)
     parser.add_argument('--input_size', default=96, type=int)
     parser.add_argument('--output_size', default=24, type=int)
     parser.add_argument('--model_type', default='m', type=str)
