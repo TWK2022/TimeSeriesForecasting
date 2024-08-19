c877cd5e77b7d6248b0bbeec26f80bab3a8cf649
twk <1024565378@qq.com>
2024-08-19 20:45:25 +0800

    !

--- economy/tushare/industry_choice.py
+++ economy/tushare/industry_choice.py
@@ -10,7 +10,7 @@ parser.add_argument('--yaml_path', default='number_all.yaml', type=str, help='|�
 parser.add_argument('--reserve_path', default='reserve.yaml', type=str, help='|自选股票信息(可选)|')
 parser.add_argument('--remove_path', default='remove.yaml', type=str, help='|要去除的股票信息(可选)|')
 parser.add_argument('--save_path', default='number.yaml', type=str, help='|保存位置|')
-parser.add_argument('--industry', default='无人驾驶,流感,新冠特效药,抗原检测,环保设备,新能源汽车,民爆概念,天然气,油气开采及服务,贵金属,游戏', type=str, help='|行业或概念，如"A,B,C"|')
+parser.add_argument('--industry', default='无人驾驶,华为海思概念股,天然气,油气开采及服务,贵金属,游戏,云游戏,网络游戏,跨境支付(CIPS),数字货币,猴痘概念', type=str, help='|行业或概念，如"A,B,C"|')
 parser.add_argument('--drop_st', default=True, type=bool, help='|是否去除ST股票|')
 args = parser.parse_args()
 args.industry = args.industry.split(',')
