c646bc35024b2857d48637a40b7ac7bf3dcb469c
TWK <109967047+TWK2022@users.noreply.github.com>
2025-04-22 16:59:42 +0800

    !

--- predict_onnx.py
+++ predict_onnx.py
@@ -1,136 +1,89 @@
 import os
-import time
 import argparse
 import onnxruntime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from train_class import train_class
 
 # -------------------------------------------------------------------------------------------------------------------- #
-parser = argparse.ArgumentParser(description='|onnx模型推理|')
-parser.add_argument('--model_path', default='best.onnx', type=str, help='|onnx模型位置|')
-parser.add_argument('--data_path', default=r'./dataset/sin_cos.csv', type=str, help='|数据位置|')
-parser.add_argument('--input_column', default='sin,cos', type=str, help='|选择输入的变量，可传入.txt|')
-parser.add_argument('--output_column', default='mix', type=str, help='|选择预测的变量，可传入.txt|')
-parser.add_argument('--input_size', default=96, type=int, help='|输入长度|')
-parser.add_argument('--output_size', default=24, type=int, help='|输出长度|')
-parser.add_argument('--batch', default=1, type=int, help='|输入图片批量，要与导出的模型对应|')
-parser.add_argument('--device', default='cuda', type=str, help='|推理设备|')
-parser.add_argument('--float16', default=True, type=bool, help='|推理数据类型，要与导出的模型对应，False时为float32|')
-parser.add_argument('--plot_len', default=1000, type=int, help='|画图长度，取数据的倒数plot_len个|')
-args = parser.parse_args()
-args.input_column = train_class.read_column(args.input_column)  # column处理
-args.output_column = train_class.read_column(args.output_column)  # column处理
-args.save_path = 'save_image'
+# 模型预测: 用预测的序列的中间值画图
 # -------------------------------------------------------------------------------------------------------------------- #
-if not os.path.exists(args.save_path):
-    os.makedirs(args.save_path)
+parser = argparse.ArgumentParser(description='|模型预测|')
+parser.add_argument('--model_path', default='best.onnx', type=str, help='|模型位置|')
+parser.add_argument('--data_path', default='dataset/ETTh1.csv', type=str, help='|数据位置|')
+parser.add_argument('--input_len', default=96, type=int, help='|输入时间长度|')
+parser.add_argument('--output_len', default=24, type=int, help='|输出时间长度|')
+parser.add_argument('--input_column', default='HUFL,HULL,MUFL,MULL,LUFL,LULL,OT', type=str, help='|输入变量，可为.txt|')
+parser.add_argument('--output_column', default='HUFL,HULL,MUFL,MULL,LUFL,LULL,OT', type=str, help='|输出变量，可为.txt|')
+parser.add_argument('--save_dir', default='draw_predict', type=str, help='|画图保存位置|')
+parser.add_argument('--pred_len', default=1000, type=int, help='|预测和画图长度|')
+parser.add_argument('--batch', default=1, type=int, help='|输入图片批量|')
+parser.add_argument('--device', default='cpu', type=str, help='|设备|')
+parser.add_argument('--float16', default=True, type=bool, help='|数据类型|')
+args, _ = parser.parse_known_args()  # 防止传入参数冲突，替代args = parser.parse_args()
+if not os.path.exists(args.save_dir):
+    os.makedirs(args.save_dir)
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-def draw(pred_middle, pred_last, true, middle, last):  # pred为预测值，true为真实值，pred和true长度不相等
-    # 画图(所有预测中值和预测末值)
-    middle_plot = np.zeros(true.shape)
-    last_plot = np.zeros(true.shape)
-    middle_plot[:, args.input_size + middle - 1:-middle] = pred_middle
-    last_plot[:, args.input_size + last - 1:] = pred_last
-    input_cut = max(args.input_size, 0)  # 防止输入序列太长时画图不好看
-    true = true[:, input_cut:]
-    middle_plot = middle_plot[:, input_cut:]
-    last_plot = last_plot[:, input_cut:]
-    for i in range(len(args.output_column)):
-        name = f'{args.output_column[i]}_last{args.plot_len}'
-        plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
-        plt.rcParams['axes.unicode_minus'] = False  # 使用字体时让坐标轴正常显示负号
-        plt.title(name)
-        plt.plot(true[i, :], color='green', label=f'{args.output_column[i]}_true')
-        plt.plot(middle_plot[i, :], color='orange', label=f'{args.output_column[i]}_{middle}')
-        plt.plot(last_plot[i, :], color='red', label=f'{args.output_column[i]}_{last}')
-        plt.grid()
-        plt.legend()
-        plt.savefig(args.save_path + '/' + name + '.jpg')
-        plt.close()
+class predict_class:
+    def __init__(self, args=args):
+        args.input_column = predict_class.read_column(args.input_column)  # column处理
+        args.output_column = predict_class.read_column(args.output_column)  # column处理
+        self.args = args
+        self.device = args.device
+        self.float16 = args.float16
+        provider = 'CUDAExecutionProvider' if args.device.lower() in ['gpu', 'cuda'] else 'CPUExecutionProvider'
+        self.model = onnxruntime.InferenceSession(args.model_path, providers=[provider])  # 加载模型和框架
+        self.input_name = self.model.get_inputs()[0].name  # 获取输入名称
+        self.output_name = self.model.get_outputs()[0].name  # 获取输出名称
 
+    @staticmethod
+    def read_column(column_file):  # column处理
+        if os.path.exists(column_file):
+            with open(column_file, encoding='utf-8') as f:
+                column = [_.strip() for _ in f.readlines()]
+        else:
+            column = column_file.split(',')
+        return column
 
-def draw_predict(last_data, last_output):
-    # 画图(对最后一组数据预测)
-    pred = np.zeros(last_data.shape)
-    pred[:, -args.output_size:] = last_output
-    true = last_data[:, -args.output_size - args.input_size:]
-    pred = pred[:, -args.output_size - args.input_size:]
-    input_cut = max(args.input_size - 50, 0)  # 防止输入序列太长时画图不好看
-    true = true[:, input_cut:]
-    pred = pred[:, input_cut:]
-    for i in range(len(args.output_column)):
-        name = f'{args.output_column[i]}_last_predict'
-        plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
-        plt.rcParams['axes.unicode_minus'] = False  # 使用字体时让坐标轴正常显示负号
-        plt.title(name)
-        plt.plot(true[i, :], color='green', label=f'{args.output_column[i]}_true')
-        plt.plot(pred[i, :], color='cyan', label=f'{args.output_column[i]}_pred')
-        plt.grid()
-        plt.legend()
-        plt.savefig(args.save_path + '/' + name + '.jpg')
-        plt.close()
+    def predict(self, data_path=args.data_path):
+        args = self.args
+        # 数据
+        df = pd.read_csv(data_path, encoding='utf-8', index_col=0)
+        input_data = df[args.input_column].values.astype(np.float32)[-args.pred_len - args.input_len:]
+        output_data = df[args.output_column].values.astype(np.float32)[-args.pred_len - args.input_len:]
+        # 预测
+        label_middle = []
+        pred_middle = []
+        half = args.output_len // 2
+        for index in range(0, len(input_data) - args.input_len - args.output_len + 1):
+            input_ = input_data[index:index + args.input_len]
+            output_ = output_data[index + args.input_len:index + args.input_len + args.output_len]
+            input_ = input_[np.newaxis].astype(np.float16 if self.float16 else np.float32)
+            pred = self.model.run([self.output_name], {self.input_name: input_})[0][0]
+            label_middle.append(output_[half])
+            pred_middle.append(pred[half])
+        label_middle = np.array(label_middle)
+        pred_middle = np.array(pred_middle)
+        self._draw(label_middle, pred_middle)
 
-
-def predict_onnx():
-    # 加载模型
-    provider = 'CUDAExecutionProvider' if args.device.lower() in ['gpu', 'cuda'] else 'CPUExecutionProvider'
-    model = onnxruntime.InferenceSession(args.model_path, providers=[provider])  # 加载模型和框架
-    input_name = model.get_inputs()[0].name  # 获取输入名称
-    output_name = model.get_outputs()[0].name  # 获取输出名称
-    print(f'| 模型加载成功:{args.model_path} |')
-    # 加载数据
-    try:
-        df = pd.read_csv(args.data_path, encoding='utf-8', index_col=0)
-    except:
-        df = pd.read_csv(args.data_path, encoding='gbk', index_col=0)
-    input_data = np.array(df[args.input_column]).astype(np.float32).T
-    input_data = input_data[:, -args.plot_len - args.input_size:]  # 限定长度方便画图
-    output_data = np.array(df[args.output_column]).astype(np.float32).T
-    last_data = output_data[:, -args.input_size - args.output_size:]
-    output_data = output_data[:, -args.plot_len - args.input_size:]  # 限定长度方便画图
-    # 数据处理
-    input_data = input_data.astype(np.float16 if args.float16 else np.float32)
-    input_len = input_data.shape[1] - args.input_size - args.output_size + 1
-    input_batch = [input_data[:, _:_ + args.input_size] for _ in range(input_len)]
-    input_batch = np.stack(input_batch, axis=0)
-    # 推理
-    start_time = time.time()
-    middle = args.output_size // 2
-    last = args.output_size
-    pred_middle = []
-    pred_last = []
-    n = input_len // args.batch
-    if n > 0:  # 如果预测数量>=批量(分批预测)
-        for i in range(n):
-            batch = input_batch[i * args.batch:(i + 1) * args.batch]
-            pred_batch = model.run([output_name], {input_name: batch})[0]
-            pred_middle.append(pred_batch[:, :, middle - 1])
-            pred_last.append(pred_batch[:, :, last - 1])
-        if input_len % args.batch > 0:  # 如果图片数量没有刚好满足批量
-            batch = input_batch[(i + 1) * args.batch:]
-            pred_batch = model.run([output_name], {input_name: batch})[0]
-            pred_middle.append(pred_batch[:, :, middle - 1])
-            pred_last.append(pred_batch[:, :, last - 1])
-    else:  # 如果图片数量<批量(直接预测)
-        batch = input_batch
-        pred_batch = model.run([output_name], {input_name: batch})[0]
-        pred_middle.append(pred_batch[:, :, middle - 1])
-        pred_last.append(pred_batch[:, :, last - 1])
-    pred_middle = np.concatenate(pred_middle, axis=0).transpose(1, 0)
-    pred_last = np.concatenate(pred_last, axis=0).transpose(1, 0)
-    last_output = pred_batch[-1]
-    end_time = time.time()
-    print('| 数据:{} 批量:{} 平均耗时:{:.4f} |'.format(input_len, args.batch, (end_time - start_time) / input_len))
-    # 画图
-    draw(pred_middle, pred_last, output_data, middle, last)
-    draw_predict(last_data, last_output)
-    print(f'| 画图保存位置:{args.save_path} |')
+    def _draw(self, label_middle, pred_middle):
+        args = self.args
+        for index, name in enumerate(args.output_column):
+            save_path = f'{args.save_dir}/{name}.jpg'
+            plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
+            plt.rcParams['axes.unicode_minus'] = False  # 使用字体时让坐标轴正常显示负号
+            plt.title(name)
+            plt.plot(label_middle[:, index], color='black', label='label')
+            plt.plot(pred_middle[:, index], color='cyan', label='pred')
+            plt.grid()
+            plt.legend()
+            plt.savefig(save_path)
+            plt.close()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
-    predict_onnx()
+    model = predict_class()
+    model.predict()
